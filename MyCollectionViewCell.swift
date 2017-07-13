cd841838c60473bdaaddf18048a323c9d92312f7
Bryan Campbell <bryanjcampbell1@gmail.com>
2017-07-12 22:07:49 -0400

    first commit

---
+++ MyCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyCollectionViewCell.swift
+//  
+//
+//  Created by Bryan Campbell on 1/22/17.
+//
+//
+
+import UIKit
+class MyCollectionViewCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+}
